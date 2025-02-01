5ecce6d6cc501fbf35e677d0eda8a3659f8e6083
miawright0 <144161859+miawright0@users.noreply.github.com>
2025-02-01 22:45:18 +0000

    completed the code

--- src/Input.java
+++ src/Input.java
@@ -8,15 +8,22 @@ public class Input
         Scanner scanner = new Scanner(System.in);
 
         //Get and store the user's name. (Store as text)
+        System.out.print("Enter your name: ");
         String name;
         name = scanner.nextLine();
+
         //Get and store the user's age. (Store as a whole number)
+        System.out.print("Enter your age: ");
         int age;
         age = scanner.nextInt();
+
         //Get and store the user's weight. (Store it as a real number)
+        System.out.print("Enter your weight: ");
         double weight;
         weight = scanner.nextDouble();
+
         //Get and store if the user is a smoker. (Store it as either true or false)
+        System.out.print("Are you a smoker? (true/false): ");
         boolean smoker;
         smoker = scanner.nextBoolean();
         //Output all of the information back to the user.
